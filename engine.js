5c1989325b642bda21f792400783a7be7ce64ced
s1109296-maker <s1109296@gmail.com>
2026-04-27 14:02:15 +0530

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,1 +1,47 @@
-g
+let steps = [];
+let currentStep = -1;
+
+function addStep(type){
+  let val = document.getElementById("val").value;
+
+  if(type==="insert"){
+    structure.insert(val);
+    steps.push({
+      action: "Insert "+val,
+      state: structure.getState(),
+      line:1
+    });
+  }
+
+  if(type==="delete"){
+    structure.delete();
+    steps.push({
+      action: "Delete",
+      state: structure.getState(),
+      line:2
+    });
+  }
+
+  nextStep();
+}
+
+function nextStep(){
+  if(currentStep < steps.length-1){
+    currentStep++;
+    showStep();
+  }
+}
+
+function prevStep(){
+  if(currentStep>0){
+    currentStep--;
+    showStep();
+  }
+}
+
+function showStep(){
+  let s = steps[currentStep];
+  document.getElementById("status").innerText = s.action;
+  highlightCode(s.line);
+  render(s.state);
+}
